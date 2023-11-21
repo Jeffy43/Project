be8da20f62ea60d36b31e66e43729002de468b8d
Jeffy <jeffyp@nycstudents.net>
2023-11-20 21:37:22 -0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ import java.awt.event.KeyEvent;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //creating of panel,frame,text
-
+        //variables
         int box1 = 1;
         int box2 = 0;
         int box3 = 0;
@@ -24,14 +23,13 @@ public class Main {
         int box14 = 0;
         int box15 = 0;
         int box16 = 0;
-
-
+        //logic
         Scanner s = new Scanner(System.in);
         System.out.println(box1 + " " + box2 + " " + box3 + " " + box4);
         System.out.println(box5 + " " + box6 + " " + box7 + " " + box8);
         System.out.println(box9 + " " + box10 + " " + box11 + " " + box12);
         System.out.println(box13 + " " + box14 + " " + box15 + " " + box16);
-        //
+
         if (s.nextLine().equals("d")) {
             if (box3 != 0) {
                 if (box4 == 0) {
@@ -48,7 +46,6 @@ public class Main {
                 }
             }
             if (box1 != 0) {
-                System.out.println("ningRun");
                 if (box2 == 0 && box3 == 0 && box4 == 0) {
                     box4 = box1;
                     box1 = 0;
@@ -58,80 +55,77 @@ public class Main {
                 } else if (box2 == 0 && box3 != 0 && box4 != 0) {
                     box2 = box1;
                     box1 = 0;
-                    }
                 }
             }
+        }
         System.out.println(box1 + " " + box2 + " " + box3 + " " + box4);
         System.out.println(box5 + " " + box6 + " " + box7 + " " + box8);
         System.out.println(box9 + " " + box10 + " " + box11 + " " + box12);
         System.out.println(box13 + " " + box14 + " " + box15 + " " + box16);
-    }
-}
+
+        //creating background and buttons
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame("2048");
+        JLabel title = new JLabel("2048");
+        JButton start = new JButton("Start");
+        frame.setSize(1600, 900);
+        title.setFont(new Font("Comic Sans MS", Font.BOLD, 150));
+        title.setForeground(new java.awt.Color(218, 165, 40));
+        panel.setBackground(Color.DARK_GRAY);
+        panel.setBorder(new LineBorder(Color.white));
+        panel.setLayout(null);
+        //allows panel to take in inputs
+        panel.setFocusable(true);
+        panel.requestFocus();
+        //adds a mouse listener to start button
+        start.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                panel.setBackground(Color.PINK);
+                panel.requestFocusInWindow();
+            }
+        });
 
 
 
+        //adds a key listener to overall panel
+        panel.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                int key = e.getKeyCode();
+                if (key == KeyEvent.VK_W) {
+                    panel.setBackground(Color.WHITE);
+                }
+                //a
+                if (key == KeyEvent.VK_A) {
+                    panel.setBackground(Color.YELLOW);
+                }
+                //s
+                if (key == KeyEvent.VK_S) {
+                    panel.setBackground(Color.BLACK);
+                }
+                //d
+                if (key == KeyEvent.VK_D) {
+                    panel.setBackground(Color.GREEN);
+                }
+            }
+        });
 
 
-//
-//
-//        JPanel panel = new JPanel();
-//        JFrame frame = new JFrame("2048");
-//        JLabel title = new JLabel("2048");
-//        JButton b1 = new JButton("Start");
-//        //creating background and buttons
-//        frame.setSize(1600, 900);
-//        panel.setBackground(Color.DARK_GRAY);
-//        title.setFont(new Font("Serif",Font.BOLD,150));
-//        title.setForeground (new java.awt.Color(218,165,40));
-//        panel.setBorder(new LineBorder(Color.white));
-//        panel.setLayout(null);
-//
-//
-//        b1.setBackground(new java.awt.Color(212, 175, 55));
-//        b1.setBounds(frame.getWidth()/2 - 150,300,300,150);
-//        title.setBounds(frame.getWidth()/2 - 150,0,600,300);
-
-//        b1.addActionListener(new ActionListener());
-//
-//
-         /*public void keyPressed(KeyEvent e) {
-             int key = e.getKeyCode();
-             //w
-             if (key == 87) {
-                 Jlabel.setlocation(34, 342);
-             }
-             //a
-             if(key == 65){
-                 Jlabel.setlocation(34, 342);
-             }
-             //s
-             if(key == 83){
-                 Jlabel.setlocation(34, 342);
-             }
-             //d
-             if(key == 68){
-                 Jlabel.setlocation(34, 342);
-             }*/
-        //            frame.setVisible(false);
-//        }
-
-
-
-
-
-
-
-        // Display Code
+        start.setBackground(new java.awt.Color(212, 175, 55));
+        start.setBounds(frame.getWidth() / 2 - 150, 300, 300, 150);
+        title.setBounds(frame.getWidth() / 2 - 180, -80, 600, 300);
+        frame.setVisible(true);
         frame.add(panel);
         panel.add(title);
-        panel.add(b1);
+        panel.add(start);
         frame.setVisible(true);
         frame.show();
-
-
-
-
-
-
+        }
     }
-}
+
+
+
+
+
+
+
+
